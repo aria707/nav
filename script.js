2e69183f616a1c18063203039531da898b546c30
Aria Noveiri <arianoveiri.888@hotmail.com>
2022-09-12 01:35:36 +0300

    I designed a simple Navbar

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const burgerButton = document.querySelector(
+  '.burger-button'
+);
+const mobileNav = document.querySelector('.main-nav');
+
+burgerButton.addEventListener('click', function () {
+  mobileNav.classList.toggle('open-menu');
+});
+
+mobileNav.addEventListener('click', function (e) {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'NAV')
+    mobileNav.classList.remove('open-menu');
+});
